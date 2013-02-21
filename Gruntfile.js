8b73536d950f0355337f09f9fd20f60a7fe6b786
Radu Cotescu <cotescu@adobe.com>
2013-02-21 16:23:17 +0200

    fix #13: Change Grunt and associated plugins to use the new 0.4.0 release 	- updated grunt's and other grunt contrib plugins' version numbers

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('gruntacular');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jsdoc');
+    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
